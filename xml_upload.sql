6e09fe86940530043f6a110271509fc64ac7c1f3
chipbaber <chipbaber@yahoo.com>
2022-10-27 10:53:12 -0400

    updates to code and testing of xml load into db

--- xml_upload.sql
+++ xml_upload.sql
@@ -1,16 +1,19 @@
 declare
+t1 timestamp := systimestamp;
+t2 timestamp;
 v_content BLOB; -- Uploading to ORDS as a BLOB in binary
+blob_length INTEGER;
 v_xml clob;
 pos INTEGER := 1;
 buf INTEGER := 32767; -- max buffer size per CLOB pull in APEX
 v_temp VARCHAR2(32767);
 v_rowid VARCHAR2(100);
 
-BEGIN
 
+BEGIN
 -- put raw blob into variable
 v_content := :BODY;
-
+blob_length := DBMS_LOB.GETLENGTH(v_content);
 --if empty xml file pushed into REST throw error
 IF v_content IS NULL
   THEN
@@ -20,11 +23,11 @@ IF v_content IS NULL
 DBMS_LOB.CREATETEMPORARY(v_xml, TRUE);
 
 /*Loop through the blob to form the CLOB*/
-WHILE pos < DBMS_LOB.GETLENGTH(v_content)
+WHILE pos < blob_length
 LOOP
 --if the lenght of the file is shorter than the length of the buffer then swap in buffer length.
-if (DBMS_LOB.GETLENGTH(v_content) < buf) THEN
-buf := DBMS_LOB.GETLENGTH(v_content);
+if (blob_length < buf) THEN
+buf := blob_length;
 end if;
 -- convert blob raw to varchar and build a CLOB temp variable.
 v_temp := UTL_RAW.CAST_TO_VARCHAR2(DBMS_LOB.SUBSTR(v_content, buf, pos));
@@ -36,17 +39,19 @@ END LOOP;
 /*Insert into ADB*/
 insert into stage_xml (XML_COL) values (xmltype.createxml(v_xml)) RETURNING ROWID INTO v_rowid;
 
+
 --return post response
 owa_util.status_line(201, '', false);
 owa_util.mime_header('application/json', true);
-htp.prn('{"status": "XML Inserted","Row ID":"'||v_rowid||'","Blob File Length":"'||DBMS_LOB.GETLENGTH(v_content)||'","Clob File Length":"'||DBMS_LOB.GETLENGTH(v_xml)||'"}');
+t2 := systimestamp; --get end time
+htp.prn('{"status": "XML Inserted","Row ID":"'||v_rowid||'","Blob File Length Received":"'||blob_length||'","Clob File on Convert":"'||DBMS_LOB.GETLENGTH(v_xml)||',"Elapsed Processing Time(Seconds)":"'||TO_CHAR(t2-t1, 'SSSS.FF')||'"}');
 END IF;
 
 EXCEPTION
       WHEN NO_DATA_FOUND THEN
             owa_util.status_line(411, 'Null XML File on REST POST Error', false);
             owa_util.mime_header('application/json', true);
-            htp.prn('{"error": "411 - Null XML File on REST POST Error","message":"No xml file found in post request. Please return including the binary data in your post call."'});
+            htp.prn('{"error": "411 - Null XML File on REST POST Error","message":"No xml file found in post request. Please return including the binary data in your post call."}');
       WHEN OTHERS
       THEN
             owa_util.status_line(412, 'Unknown Exception Error', false);
