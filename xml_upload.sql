d962ecda8b6738d4817eb7aa12780889976fe371
chipbaber <chipbaber@yahoo.com>
2022-11-03 16:48:37 -0400

    adding in empty funciton to variable clob post transaction

--- xml_upload.sql
+++ xml_upload.sql
@@ -39,6 +39,8 @@ END LOOP;
 /*Insert into ADB*/
 insert into stage_xml (XML_COL, PROCESSED) values (xmltype.createxml(v_xml), 'N') RETURNING ROWID INTO v_rowid;
 
+--empty out the Clob
+ v_xml := EMPTY_CLOB();
 
 --return post response
 owa_util.status_line(201, '', false);
@@ -52,10 +54,12 @@ EXCEPTION
             owa_util.status_line(411, 'Null XML File on REST POST Error', false);
             owa_util.mime_header('application/json', true);
             htp.prn('{"error": "411 - Null XML File on REST POST Error","message":"No xml file found in post request. Please return including the binary data in your post call."}');
+            v_xml := EMPTY_CLOB();
       WHEN OTHERS
       THEN
             owa_util.status_line(412, 'Unknown Exception Error', false);
             owa_util.mime_header('application/json', true);
             htp.prn('{"error": "412 - Unknown Exception Error","status": "message","Something went wrong in the code, here is the sqlerr response:"'||SQLERRM||'}');
+             v_xml := EMPTY_CLOB();
 
 end;
